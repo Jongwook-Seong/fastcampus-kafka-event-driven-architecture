bc4b5a80ca67e0764428ba65d46ce3f84fa00cb1
Xavier Jongwook Seong <ypwa121@gmail.com>
2024-07-03 01:58:12 +0900

    learn: API 엔드포인트 생성 및 Swagger 연동1

---
+++ domain/src/main/java/com/fastcampus/kafkahandson/ugc/post/model/ResolvedPost.java
@@ -0,0 +1,39 @@
+package com.fastcampus.kafkahandson.ugc.post.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResolvedPost { // Post + 메타정보 => 조립
+
+    private Long id;
+    private String title;
+    private String content;
+    private Long userId;
+    private String userName;
+    private Long categoryId;
+    private String categoryName;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private boolean updated;
+
+    public static ResolvedPost generate(Post post, String userName, String categoryName) {
+        return new ResolvedPost(
+                post.getId(),
+                post.getTitle(),
+                post.getTitle(),
+                post.getUserId(),
+                userName,
+                post.getCategoryId(),
+                categoryName,
+                post.getCreatedAt(),
+                post.getUpdatedAt(),
+                !post.getCreatedAt().equals(post.getUpdatedAt())
+        );
+    }
+}
---
+++ usecase/post-usecase/src/test/java/com/example/postusecase/PostUsecaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postusecase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostUsecaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
